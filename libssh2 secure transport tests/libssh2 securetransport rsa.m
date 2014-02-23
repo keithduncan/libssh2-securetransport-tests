48c259669a4e0be391e1e211bb89a242c6d59ce5
Keith Duncan <keith.duncan@github.com>
2014-02-23 16:56:15 +0000

    Test decoding binary PKCS#8 keys

--- libssh2 secure transport tests/libssh2 securetransport rsa.m
+++ libssh2 secure transport tests/libssh2 securetransport rsa.m
@@ -76,4 +76,9 @@ static NSData *SHA1(NSData *data) {
 	[self _testRSASignAndVerifyWithKey:[[NSBundle bundleForClass:self.class] URLForResource:@"plain_pkcs1_rsa" withExtension:@"pem"] passphrase:nil];
 }
 
+- (void)testRSAPlainPKCS8_DER
+{
+	[self _testRSASignAndVerifyWithKey:[[NSBundle bundleForClass:self.class] URLForResource:@"plain_pkcs8_rsa" withExtension:@"der"] passphrase:nil];
+}
+
 @end
